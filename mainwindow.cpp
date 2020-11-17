5ea82ee8d83fd9455e39bed1d7e63c98ac9e01eb
Wicem Benina <benina.wicem@gmail.com>
2020-11-17 08:33:35 +0100

    2nd Commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,55 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include<QComboBox>
+#include<QGridLayout>
+#include<QGroupBox>
+#include<QLabel>
+#include<QLineEdit>
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+: QWidget(parent)
 {
-    ui->setupUi(this);
+//! [1]
+QGroupBox *echoGroup = new QGroupBox(tr("Echo"));
+QLabel *echoLabel = new QLabel(tr("Mode:"));
+QComboBox *echoComboBox = new QComboBox;
+echoComboBox->addItem(tr("Normal"));
+echoComboBox->addItem(tr("Password"));
+echoComboBox->addItem(tr("PasswordEchoOnEdit"));
+echoComboBox->addItem(tr("No Echo"));
+echoLineEdit = new QLineEdit;
+echoLineEdit->setPlaceholderText("Entrer votre texte");
+//! [1]
+//! [2]
+QGridLayout *echoLayout = new QGridLayout;
+echoLayout->addWidget(echoLabel, 0, 0);
+echoLayout->addWidget(echoComboBox, 0, 1);
+echoLayout->addWidget(echoLineEdit, 1, 0, 1, 2);
+echoGroup->setLayout(echoLayout);
+//! [2]
+//! [3]
+QGridLayout *layout = new QGridLayout;
+layout->addWidget(echoGroup, 0, 0);// 0,0 définit la position de notre layout
+setLayout(layout);
+setWindowTitle(tr("Line Edits"));// définit le titre de l'application
+}
+//! [3]
+//! [4]
+void MainWindow::echoChanged(int index)
+{
+switch (index) {
+case 0:
+echoLineEdit->setEchoMode(QLineEdit::Normal);
+break;
+case 1:
+echoLineEdit->setEchoMode(QLineEdit::Password);
+break;
+case 2:
+echoLineEdit->setEchoMode(QLineEdit::PasswordEchoOnEdit);
+break;
+case 3:
+echoLineEdit->setEchoMode(QLineEdit::NoEcho);
+break;
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+    }
+    //! [4]
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
-#include <QMainWindow>
-
+#include <QWidget>
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+class QLineEdit;
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+//! [0]
+class MainWindow : public QWidget
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+MainWindow(QWidget *parent = nullptr);
 
+public slots:
+void echoChanged(int);
 private:
-    Ui::MainWindow *ui;
+QLineEdit *echoLineEdit;
 };
+//! [0]
+
 #endif // MAINWINDOW_H
