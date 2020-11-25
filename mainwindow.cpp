3e98955265f1cf27766e6813cbcf5713d4a059eb
ThavaNix <thavaprasanthnavaraththinam@gmail.com>
2020-11-25 21:14:30 +0100

    'oral'

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,71 +1,90 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include<QComboBox>
-#include<QGridLayout>
-#include<QGroupBox>
-#include<QLabel>
-#include<QLineEdit>
+#include <QComboBox>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
 #include <QString>
-MainWindow::MainWindow(QWidget *parent)
-: QWidget(parent)
+#include <QMessageBox>
+#include <QString>
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
 {
-//! [1]
-QGroupBox *echoGroup = new QGroupBox(tr("Echo"));
-QGroupBox *PaveNum =   new QGroupBox( tr("PaveNum"));
-QLabel *echoLabel = new QLabel(tr("Mode:"));
-QComboBox *echoComboBox = new QComboBox;
-echoComboBox->addItem(tr("Normal"));
-echoComboBox->addItem(tr("Password"));
-echoComboBox->addItem(tr("PasswordEchoOnEdit"));
-echoComboBox->addItem(tr("No Echo"));
+
+QGroupBox *echoGroup = new QGroupBox;
+
+PaveNum = new QGroupBox("Utilisez Le Pavé Numerique Pour le MDP");
+PaveNum->setStyleSheet("*{font-size : 14px ; color : blue;}");
+
+QLabel *echoLabel = new QLabel(tr("Connexion :"));
+loginAlert = new QLabel;
+
 echoLineEdit = new QLineEdit;
 echoLineEdit2 = new QLineEdit;
+echoLineEdit2->setMaxLength(3);
+echoLineEdit->setMaxLength(3);
+echoLineEdit2->setDisabled(true);
+echoLineEdit2->setVisible(false);
+
+echoCheckBox = new QCheckBox("Masquer");
+
+echoCheckBox->setVisible(false);
+echoCheckBox->setCheckState(Qt::Checked);
+echoLineEdit->setStyleSheet("*{color : black}");
+echoLineEdit2->setStyleSheet("*{color : black}");
 
 echoLineEdit->setPlaceholderText("ID");
 
 echoLineEdit2->setPlaceholderText("MotDePasse");
-//! [1]
-//! [2]
+echoLineEdit2->setEchoMode(QLineEdit::Password);
+
+
 QGridLayout *echoLayout = new QGridLayout;
 
 echoLayout->addWidget(echoLabel, 0, 0);
-echoLayout->addWidget(echoComboBox, 0, 1);
 echoLayout->addWidget(echoLineEdit, 1, 0, 1, 2);
 echoLayout->addWidget(echoLineEdit2, 2, 0, 1, 2);
-
-////////////////////////////////////////
-//if (echoLineEdit->hasFocus()){}else
-//{
-//    qDebug("Identifiant");
-//}
+echoLayout->addWidget(echoCheckBox,2 , 3);
 
 
-/////////////////////////////////////////
 
 echoGroup->setLayout(echoLayout);
+echoGroup->setStyleSheet("*{font-size : 14px;}");
 //! [2]
 //! [3]
 QGridLayout *layout = new QGridLayout;
-layout->addWidget(echoGroup, 0, 0);// 0,0 définit la position de notre layout
+layout->addWidget(echoGroup, 0, 0);
 layout->addWidget(PaveNum , 1 , 0);
+
+PaveNum->setVisible(false);
 setLayout(layout);
-setWindowTitle(tr("Line Edits"));// définit le titre de l'application
+setWindowTitle(tr("Fenetre de connexion"));
 
 
 QGridLayout *pavNum = new QGridLayout;
-this->B0->setGeometry(50, 100, 100, 50);
-pavNum->addWidget(B0 , 0 , 1 );
-pavNum->addWidget(B1 , 0 , 2);
-pavNum->addWidget(B2 , 0 , 3);
-pavNum->addWidget(B3 , 1 , 1);
-pavNum->addWidget(B4 , 1 , 2);
-pavNum->addWidget(B5 , 1 , 3);
-pavNum->addWidget(B6 , 2 , 1);
-pavNum->addWidget(B7 , 2 , 2 );
-pavNum->addWidget(B8 , 2 , 3);
-pavNum->addWidget(B9 , 3 , 2);
+
+pavNum->addWidget(B0 , 3 ,1 );
+pavNum->addWidget(B1 , 0 , 0);
+pavNum->addWidget(B2 , 0 , 1);
+pavNum->addWidget(B3 , 0 , 2);
+pavNum->addWidget(B4 , 1 , 0);
+pavNum->addWidget(B5 , 1 , 1);
+pavNum->addWidget(B6 , 1 , 2);
+pavNum->addWidget(B7 , 2 , 0);
+pavNum->addWidget(B8 , 2 , 1);
+pavNum->addWidget(B9 , 2 , 2);
+pavNum->addWidget(bConn , 3 , 2);
+pavNum->addWidget(bClose , 3 , 0);
+pavNum->addWidget(loginAlert ,4 ,1);
+
+
+loginAlert->setVisible(false);
+
+bConn->setStyleSheet("* { background-color: #5294e2  ; color : black}");
+bClose->setStyleSheet("*{background-color: #853934 ; color : black}");
 PaveNum->setLayout(pavNum);
+
 
 
 //! [5]
@@ -81,43 +100,90 @@ connect( B7 , SIGNAL(clicked()),this, SLOT(buttonClicked()));
 connect( B8 , SIGNAL(clicked()),this, SLOT(buttonClicked()));
 connect( B9 , SIGNAL(clicked()),this, SLOT(buttonClicked()));
 
-connect(echoComboBox, QOverload<int>::of(&QComboBox::activated),
-this, &MainWindow::echoChanged);
-//! [5]
-}
-//! [3]
 
-void MainWindow::echoChanged(int index)
-{
-switch (index) {
-case 0:
-echoLineEdit->setEchoMode(QLineEdit::Normal);
-break;
-case 1:
-echoLineEdit->setEchoMode(QLineEdit::Password);
-break;
-case 2:
-echoLineEdit->setEchoMode(QLineEdit::PasswordEchoOnEdit);
-break;
-case 3:
-echoLineEdit->setEchoMode(QLineEdit::NoEcho);
-break;
-}
+connect( bConn , SIGNAL(clicked()),this, SLOT(slotbConn()));
+
+connect( bClose , SIGNAL(clicked()),this, SLOT(slotbClose()));
+
+connect( echoCheckBox , SIGNAL(clicked()),this, SLOT(echoChanged()));
+
+connect(echoLineEdit,SIGNAL(textChanged(QString)),this,SLOT(idVerification()));
+
+
+//connect(echoLineEdit2,SIGNAL(textChanged(QString)),this,SLOT(mdpVerification()));
+
 
 }
+
+
 
 void MainWindow::buttonClicked()
  {
+    QPushButton *button = (QPushButton *)sender();
+       int num_button = (button->text()[0].digitValue());
 
-    QPushButton *button = (QPushButton *)sender() ;
-    int num_bouton = (button->text()[0].digitValue());
- if (echoLineEdit2->focusWidget()){
- echoLineEdit2->setText(echoLineEdit2->text()+QString::number(num_bouton));
 
- }
- else if (echoLineEdit->focusWidget()) {
-      echoLineEdit->setText(echoLineEdit->text()+QString::number(num_bouton));
- }
+       if(echoLineEdit2->isEnabled())
+         {
+             echoLineEdit2->setText(echoLineEdit2->text()+QString::number(num_button));
+         }
+}
+
+void MainWindow::echoChanged()
+{
+    if(echoCheckBox->isChecked()){
+     echoLineEdit2->setEchoMode(QLineEdit::Password);
+}
+    else if (!echoCheckBox->isChecked()){
+     echoLineEdit2->setEchoMode(QLineEdit::Normal);
+    }
+}
+
+void MainWindow::idVerification(){
+    if(echoLineEdit->text()=="000")
+    {
+        echoLineEdit->setDisabled(true);
+        echoLineEdit->setVisible(false);
+        echoLineEdit2->setDisabled(false);
+        echoLineEdit2->setVisible(true);
+        echoCheckBox->setVisible(true);
+        PaveNum->setVisible(true);
+
+
+    }
+
+}
+
+void MainWindow::slotbConn(){
+
+
+
+    if (echoLineEdit2->text() == "000")
+        {
+
+            loginAlert->setVisible(true);
+            loginAlert->setText("Connexion Réussi");
+            loginAlert->setStyleSheet("*{color:green}");
+        }
+        else if (echoLineEdit2->text() != "000"){
+
+        loginAlert->setVisible(true);
+          loginAlert->setText("Connexion Refuser" );
+          loginAlert->setStyleSheet("*{color:red}");
+          echoLineEdit2->clear();
+    }
 
 
 }
+
+
+
+void MainWindow::slotbClose(){
+this->close();
+}
+//void MainWindow::mdpVerification(){}
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 #include <QWidget>
 #include <QPushButton>
+#include <QCheckBox>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLabel>
 QT_BEGIN_NAMESPACE
 class QLineEdit;
 QT_END_NAMESPACE
@@ -13,6 +17,21 @@ Q_OBJECT
 
 public:
 MainWindow(QWidget *parent = nullptr);
+
+public slots:
+
+void buttonClicked();
+void echoChanged();
+void idVerification();
+void slotbConn();
+void slotbClose();
+//void mdpVerification();
+private:
+QLineEdit *echoLineEdit;
+QLineEdit *echoLineEdit2;
+QCheckBox *echoCheckBox;
+QGroupBox *PaveNum;
+QLabel *loginAlert;
 QPushButton *B0 = new QPushButton("0") ;
 QPushButton *B1 = new QPushButton("1") ;
 QPushButton *B2 = new QPushButton("2") ;
@@ -23,14 +42,8 @@ QPushButton *B6 = new QPushButton("6") ;
 QPushButton *B7 = new QPushButton("7") ;
 QPushButton *B8 = new QPushButton("8") ;
 QPushButton *B9 = new QPushButton("9") ;
-public slots:
-void echoChanged(int);
-
-void buttonClicked();
-
-private:
-QLineEdit *echoLineEdit;
-QLineEdit *echoLineEdit2;
+QPushButton *bConn = new QPushButton("Connexion") ;
+QPushButton *bClose = new QPushButton("Fermer");
 };
 //! [0]
 
